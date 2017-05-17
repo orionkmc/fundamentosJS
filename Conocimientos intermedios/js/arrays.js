84e6427c7ed834e5bbe0312423b3dcf653f99762
Kuai-mare <orionkmc@gmail.com>
2017-05-17 14:35:39 -0400

    operaciones con arrays

---
+++ Conocimientos intermedios/js/arrays.js
@@ -0,0 +1,31 @@
+function suma(...numeros){
+    return numeros.reduce(function reduce(acum, numero){
+        acum += numero;
+        return acum;
+    }, 0);
+}
+
+function dobles(...numeros){
+    return numeros.map(function(numero){
+        return numero * 2;
+    });
+}
+
+function pares(...numeros){
+    return numeros.filter(function(numero){
+        return numero % 2 == 0;
+    }); 
+}
+
+function max(...numeros){
+    let maximo = numeros[0];
+    numeros.find(function(numero){
+        (numero > maximo) ? maximo = numero : '';
+    });
+    return maximo;
+}
+
+console.log(suma(4, 6, 12, 8, 5));
+console.log(dobles(4, 8, 12, 8954, 7, 2));
+console.log(pares(10, 2, 3))
+console.log(max(10, 22, 39))
