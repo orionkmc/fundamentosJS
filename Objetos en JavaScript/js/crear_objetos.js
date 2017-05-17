94f3ac2339c73213c95417d1c5062e455fcbad3b
Kuai-mare <orionkmc@gmail.com>
2017-05-17 12:23:41 -0400

    creando objetos con Object.create

---
+++ Objetos en JavaScript/js/crear_objetos.js
@@ -0,0 +1,28 @@
+const Punto = {
+    init: function moverEnX(x, y){
+        this.x = x;
+        this.y = y;
+    },
+    moverEnX: function moverEnX(x){ this.x += x; },
+    moverEnY: function moverEnY(y){ this.y += y; },
+    distancia: function distancia(p) {
+        const x = this.x -p.x;
+        const y = this.y -p.y;
+
+        return Math.sqrt(x * x + y * y);
+    }
+}
+
+const p1 = Object.create(Punto);
+const p2 = Object.create(Punto);
+
+p1.init(0, 4);
+p2.init(3, 0);
+
+console.log(p1.distancia(p2));
+console.log(p2.distancia(p1));
+p1.moverEnX(10);
+p2.moverEnX(1);
+p2.moverEnY(10);
+console.log(p1.distancia(p2));
+console.log(p2.distancia(p1)); 
