b324ab4df08a3758f8cdb61f68047694443dbb8d
Kuai-mare <orionkmc@gmail.com>
2017-05-17 18:03:11 -0400

    usando closures

---
+++ Conocimientos intermedios/js/closures.js
@@ -0,0 +1,27 @@
+function saludarFamilia(apellido){
+    return function saludarMiembroDeFamilia(nombre){
+        console.log(`Hola ${nombre} ${apellido}`)
+    }
+}
+
+const saludarGomez = saludarFamilia("Gomez");
+const saludarPerez = saludarFamilia("Perez");
+const saludarRomero = saludarFamilia("Romero");
+
+saludarGomez("Pedro");
+saludarGomez("Juan");
+saludarGomez("Laura");
+saludarGomez("Mónica");
+
+saludarPerez("Dario");
+saludarPerez("Martin");
+saludarPerez("Julieta");
+
+saludarRomero("Jorge");
+
+/*Otro Ejemplo */ 
+    let makePrefixer = (prefixer) => {
+        return function union(resto_nombre){
+            console.log(`${prefixer}${resto_nombre}`)
+        }
+    }
