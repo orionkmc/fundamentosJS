d81b2cd624ca70b8fe8f3319f26b44cb5acf86a6
Kuai-mare <orionkmc@gmail.com>
2017-05-19 15:17:34 -0400

    Secuencia de fibonacci utilizando recursividad

--- 6.-Estructuras de datos y funciones avanzadas/js/recursividad.js
+++ 6.-Estructuras de datos y funciones avanzadas/js/recursividad.js
@@ -1,8 +1,12 @@
-// secuencia de fibonacci
-function fibonacci(num){
+// secuencia de fibonacciRecur
+let contRecur = 0;
+function fibonacciRecur(num){
     if (num == 1) return 0;
     if (num == 2) return 1;
-    return fibonacci(num - 1) + fibonacci(num - 2);
-}
 
-console.log(fibonacci(8)); // 13
+    contRecur++;
+    return fibonacciRecur(num - 1) + fibonacciRecur(num - 2);
+}
+console.log(`fibonacci recursuvo`);
+console.log(`El fibonacci de 20 es ${fibonacciRecur(20)}`); // 
+console.log(`Se invoco la funcion ${contRecur} veces`);
